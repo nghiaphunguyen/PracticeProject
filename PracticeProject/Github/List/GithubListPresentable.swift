3df00a44432f6635ff5a87e663ac1895a0163fbd
Nghia Nguyen <nghia.nguyen@thecarousell.com>
2020-02-29 23:02:40 +0700

    init GithubListViewController

---
+++ PracticeProject/Github/List/GithubListPresentable.swift
@@ -0,0 +1,5 @@
+//
+//  Copyright © 2020 Nghia Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ PracticeProject/Github/List/GithubListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GithubListViewController.swift
+//  PracticeProject
+//
+//  Created by Nghia Nguyen on 2/29/20.
+//  Copyright © 2020 Nghia Nguyen. All rights reserved.
+//
+
+import UIKit
+
+final class GithubListViewController: UIViewController {
+    
+}
